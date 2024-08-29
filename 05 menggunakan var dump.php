d31dd5333b1c31327b5c130dcb678c1e36c93f4a
sasaaaaaaaaaaaaaa <alikanazwasabila5@gmail.com>
2024-08-29 12:13:43 +0700

    var_dump string

--- 05 menggunakan var dump.php
+++ 05 menggunakan var dump.php
@@ -6,5 +6,5 @@ $variabel_array = array(1, 2, "tiga", 4.5);
 var_dump($variabel_string);
 var_dump($variabel_integer);
 var_dump($variabel_array);
-print_r ("<p><strong>by :alika naswa sabila");
+print_r ("<p><strong>by :alika naswa sabilaa");
 ?>
