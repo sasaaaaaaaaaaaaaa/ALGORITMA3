c8f733f46d97c935224358411d839d22598e4d51
sasaaaaaaaaaaaaaa <alikanazwasabila5@gmail.com>
2024-08-30 15:42:29 +0700

    membuat pernyataan if

---
+++ 45 Pernyataan If.php
@@ -0,0 +1,6 @@
+<?php
+$nilai = 75;
+if ($nilai > 70) {
+  echo "Selamat datang di sekolah smk 1 karang baru!";
+}
+print_r ("<p><strong>by :alika naswa sabila");
---
+++ 46 Pernyataan Else.php
@@ -0,0 +1,6 @@
+$nilai = 60;
+if ($nilai > 70) {
+  echo "Selamat, kamu lulus!";
+} else {
+  echo "Maaf, kamu harus mengulang.";
+}
