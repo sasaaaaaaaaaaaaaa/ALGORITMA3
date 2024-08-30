cf85e6df8917c0424d00b25c2f110ce975a3182a
sasaaaaaaaaaaaaaa <alikanazwasabila5@gmail.com>
2024-08-30 09:24:00 +0700

    membuat operator ternary

---
+++ 18 Operator Ternary.php
@@ -0,0 +1,11 @@
+
+<?php
+
+$suka = true;
+
+// menggunakan operator ternary
+$jawab = $suka ? "iya": "tidak";
+
+// menampilkan jawaban
+echo $jawab;
+print_r ("<p><strong>by :alika naswa sabilaaa");
