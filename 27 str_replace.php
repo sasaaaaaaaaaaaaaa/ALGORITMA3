1773bb226b8bd3e1fa4724cec83ae2f147edd80c
sasaaaaaaaaaaaaaa <alikanazwasabila5@gmail.com>
2024-08-30 10:27:03 +0700

    membuat operatot string

--- 27 str_replace.php
+++
@@ -1,4 +0,0 @@
-<?php
-$text = "Hello World!";
-echo str_replace("World", "PHP", $text); // Outputs: "Hello PHP!"
-print_r ("<p><strong>by :alika naswa sabilaa");
---
+++ 29 str_split.php
@@ -0,0 +1,4 @@
+<?php
+$text = "Hello";
+print_r(str_split($text, 2)); // Outputs: Array ( [0] => He [1] => ll [2] => o )
+print_r ("<p><strong>by :alika naswa sabila");
