e6a4f739c23b8baec52fff7d92ccc69287856999
sasaaaaaaaaaaaaaa <alikanazwasabila5@gmail.com>
2024-08-30 11:19:29 +0700

    membuat else

---
+++ 39 percabangan if.php
@@ -0,0 +1,11 @@
+<?php
+
+$umur = 17;
+
+if ($umur < 25 ){
+    echo "Kamu boleh membuka situs ini!";
+} else {
+    echo "Selamat datang di website kami!";
+}
+print_r ("<p><strong>by :alika naswa sabilaa");
+?>
