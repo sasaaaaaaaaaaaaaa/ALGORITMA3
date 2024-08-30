742b3c8c59e0c834e86f0aabcce67978aa253372
sasaaaaaaaaaaaaaa <alikanazwasabila5@gmail.com>
2024-08-30 10:38:20 +0700

    membuat round

---
+++ 30 OPERATOR NUMBERS.php
@@ -0,0 +1,6 @@
+<?php
+echo round(3.4); // Hasil: 3
+echo round(3.5); // Hasil: 4
+echo round(3.6); // Hasil: 4
+echo round(3.678, 2); // Hasil: 3.68
+print_r ("<p><strong>by :alika naswa sabilaa");
