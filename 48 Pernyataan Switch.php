86f37ab1160f4d0170988064342e46fd03332c9c
sasaaaaaaaaaaaaaa <alikanazwasabila5@gmail.com>
2024-08-30 15:52:44 +0700

    membuat pernyataan switch

---
+++ 48 Pernyataan Switch.php
@@ -0,0 +1,14 @@
+<?php
+$grade = 'B';
+switch ($grade) {
+  case 'A':
+    echo "Luar biasa!";
+    break;
+  case 'B':
+  case 'C':
+    echo "sangat bagus!";
+    break;
+  default:
+    echo "Tetap semangat!";
+}
+print_r ("<p><strong>by :alika naswa sabila");
