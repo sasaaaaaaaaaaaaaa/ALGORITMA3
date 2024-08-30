71941d1d028626640f0c2f766038022d5268459b
sasaaaaaaaaaaaaaa <alikanazwasabila5@gmail.com>
2024-08-30 10:31:27 +0700

    membuat substr

--- 27 substr.php
+++ 27 substr.php
@@ -2,4 +2,4 @@
 $text = "Hello World!";
 echo substr($text, 0, 5); // Outputs: "Hello"
 echo substr($text, 6); // Outputs: "World!"
-print_r ("<p><strong>by :alika naswa sabilaaa");
+print_r ("<p><strong>by :alika naswa sabilaa");
