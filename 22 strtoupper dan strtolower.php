a7df00876fbd38126b7fa4f73679e3e97e084c9a
sasaaaaaaaaaaaaaa <alikanazwasabila5@gmail.com>
2024-08-30 10:28:18 +0700

    membuat strtoupper

--- 22 strtoupper dan strtolower.php
+++ 22 strtoupper dan strtolower.php
@@ -1,4 +1,4 @@
 <?php
 echo strtoupper("Hello World!"); // Outputs: "HELLO WORLD!"
 echo strtolower("Hello World!"); // Outputs: "hello world!"
-print_r ("<p><strong>by :alika naswa sabilaa");
+print_r ("<p><strong>by :alika naswa sabila");
