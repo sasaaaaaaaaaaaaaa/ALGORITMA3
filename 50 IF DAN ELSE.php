a402a71875a2ed68455bf875dc6cfafcc7a56a51
sasaaaaaaaaaaaaaa <alikanazwasabila5@gmail.com>
2024-08-30 16:17:33 +0700

    membuat if dan else

--- 50 IF DAN ELSE.php
+++ 50 IF DAN ELSE.php
@@ -3,7 +3,8 @@
 $umur = 20;
 
 if ($umur >= 17) {
-    echo "Kamu sudah cukup umur.";
+    echo "Kamu sudah dewasa.";
+    echo "<br><hr>";
 }
 //Contoh Penggunaan If dan Else
 $umur = 16;
@@ -12,6 +13,7 @@ if ($umur >= 17) {
     echo "Kamu sudah cukup umur.";
 } else {
     echo "Kamu belum cukup umur.";
+    echo "<br><hr>";
 }
 //Kombinasi Dengan ElseIf
 $suhu = 30;
@@ -20,6 +22,8 @@ if ($suhu < 20) {
     echo "Cuaca dingin.";
 } elseif ($suhu <= 30) {
     echo "Cuaca hangat.";
+    echo "<br><hr>";
 } else {
     echo "Cuaca panas.";
 }
+print_r ("<p><strong>by :alika naswa sabilaa");
