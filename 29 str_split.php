d62cc78e40f12c9dda79a0c86b997eee8722ee28
sasaaaaaaaaaaaaaa <alikanazwasabila5@gmail.com>
2024-08-30 10:34:02 +0700

    membuat str_split

--- 29 str_split.php
+++ 29 str_split.php
@@ -1,4 +1,4 @@
 <?php
 $text = "Hello";
 print_r(str_split($text, 2)); // Outputs: Array ( [0] => He [1] => ll [2] => o )
-print_r ("<p><strong>by :alika naswa sabila");
+print_r ("<p><strong>by :alika naswa sabilaa");
