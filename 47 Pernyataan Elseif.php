e59424c86af029dde6024633199b92409e47d180
sasaaaaaaaaaaaaaa <alikanazwasabila5@gmail.com>
2024-08-30 15:49:15 +0700

    membuat prnytaan elseif

---
+++ 47 Pernyataan Elseif.php
@@ -0,0 +1,10 @@
+<?php
+$nilai = 100;
+if ($nilai >= 90) {
+  echo "Nilai kamu A";
+} elseif ($nilai >= 80) {
+  echo "Nilai kamu B";
+} else {
+  echo "Nilai kamu C";
+}
+print_r ("<p><strong>by :alika naswa sabila");
