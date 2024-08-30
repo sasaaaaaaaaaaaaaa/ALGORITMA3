f0222206a59f669539339bb4db3b5b6de0afa978
sasaaaaaaaaaaaaaa <alikanazwasabila5@gmail.com>
2024-08-30 15:45:52 +0700

    membuat penyataan else

--- 46 Pernyataan Else.php
+++ 46 Pernyataan Else.php
@@ -1,6 +1,9 @@
+<?php
 $nilai = 60;
 if ($nilai > 70) {
   echo "Selamat, kamu lulus!";
 } else {
-  echo "Maaf, kamu harus mengulang.";
+  echo "Maaf, pendaftaran sudah tutup.";
 }
+print_r ("<p><strong>by :alika naswa sabila");
+?>
